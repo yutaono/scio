f9a54109b4e89160cd4ef9959cacf0f07fec552a
Neville Li <neville@spotify.com>
2017-10-19 10:34:45 +0200

    add ScioContext#sequence

--- scio-core/src/main/scala/com/spotify/scio/streaming/package.scala
+++ scio-core/src/main/scala/com/spotify/scio/streaming/package.scala
@@ -17,7 +17,11 @@
 
 package com.spotify.scio
 
+import com.spotify.scio.util.Functions
+import com.spotify.scio.values.SCollection
+import org.apache.beam.sdk.io.GenerateSequence
 import org.apache.beam.sdk.values.WindowingStrategy.AccumulationMode
+import org.joda.time.{Duration, Instant}
 
 /**
  * Main package for streaming APIs. Import all.
@@ -34,4 +38,34 @@ package object streaming {
   /** Alias for WindowingStrategy `AccumulationMode.DISCARDING_FIRED_PANES`. */
   val DISCARDING_FIRED_PANES = AccumulationMode.DISCARDING_FIRED_PANES
 
+  /** Enchanced version of [[ScioContext]] with streaming methods. */
+  implicit class StreamingScioContext(val self: ScioContext) extends AnyVal {
+    /**
+     * Generate a sequence of longs starting from the given value, and either up to the given limit
+     * or until [[Long.MaxValue]] / until the given time elapses.
+     * @param from the minimum number to generate (inclusive)
+     * @param to the maximum number to generate (exclusive)
+     * @param interval interval between elements
+     * @param maxReadTime stop generating elements after the given time
+     * @param timestampFn function to use to assign timestamps to the elements
+     * @return
+     */
+    def sequence(from: Long, to: Long = -1,
+                 interval: Duration = null,
+                 maxReadTime: Duration = null,
+                 timestampFn: Long => Instant = null): SCollection[Long] = {
+      var t = GenerateSequence.from(from).to(to)
+      if (interval != null) {
+        t = t.withRate(1, interval)
+      }
+      if (maxReadTime != null) {
+        t = t.withMaxReadTime(maxReadTime)
+      }
+      if (timestampFn != null) {
+        val f = Functions.serializableFn(timestampFn.asInstanceOf[java.lang.Long => Instant])
+        t = t.withTimestampFn(f)
+      }
+      self.customInput("sequence", t).asInstanceOf[SCollection[Long]]
+    }
+  }
 }
